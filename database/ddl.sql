1cb88d8ecd31cdf2e91e76e02a3b017dccff195b
taketoputa <orcalinn@.gmail.com>
2024-07-31 14:28:32 +0700

    database modified

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,52 +1,79 @@
 CREATE DATABASE fabric_fushion;
-
 USE DATABASE fabric_fushion;
 
--- Table Users
-CREATE TABLE
-  users (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL
-  );
+-- table roles
+create table roles (
+    id int unsigned auto_increment primary key,
+    role varchar(10)
+);
 
--- Table Employees
-CREATE TABLE
-  employees (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    user_id INT UNSIGNED,
-    email VARCHAR(255) UNIQUE,
-    name VARCHAR(255) NOT NULL,
-    position VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id)
-  );
+-- table users
+create table users (
+    id int unsigned auto_increment primary key,
+    role_id unsigned,
+    email varchar(255) unique,
+    password varchar(255) not null,
+    foreign key (role_id) references roles(id)
+);
+
+-- table positions
+create table positions (
+    id int unsigned auto_increment primary key,
+    position varchar(10)
+);
+
+-- table employees
+create table employees (
+    id int unsigned auto_increment primary key,
+    position_id int unsigned,
+    user_id int unsigned,
+    name varchar(225),
+    foreign key (position_id) references positions(id)
+    foreign key (user_id) references users(id)
+);
+
 
 -- Table Customers
-CREATE TABLE
-  customers (
+CREATE TABLE customers (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     user_id INT UNSIGNED,
-    email VARCHAR(255) UNIQUE,
-    name VARCHAR(255) NOT NULL,
     phone_number VARCHAR(30) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id)
-  );
-
--- Table Products
-CREATE TABLE
-  products (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    category VARCHAR(100) NOT NULL
-  );
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 -- Table Sales
 CREATE TABLE
   sales (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    order_date DATETIME NOT NULL,
     customer_id INT UNSIGNED,
-    FOREIGN KEY (customer_id) REFERENCES customers (id)
+    order_date DATETIME NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
   );
+
+-- table categories
+create table categories (
+    id int unsigned auto_increment primary key,
+    category varchar(10)
+);
+
+-- Table Products
+CREATE TABLE
+  products (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    category_id int unsigned,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    foreign key (category_id) references categories(id)
+);
+
+-- Table Products
+CREATE TABLE sales_products (
+    id int unsigned auto_increment primary key,
+    sale_id int unsigned,
+    product_id int unsigned,
+    quantity int,
+    foreign key (sale_id) references sales(id),
+    foreign key (product_id) references products(id)
+);
+
