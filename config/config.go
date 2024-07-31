6e811e05dab7ed9ffdfeb8e78931e04016e755a6
harya <haryak279@gmail.com>
2024-07-31 19:54:11 +0700

    DATABASE RAILWAY

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ func DatabaseConfig() string {
 	fmt.Println("Config Done")
 
 	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
-		os.Getenv("DB_USER"),
+		os.Getenv("DB_USER"), //
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
